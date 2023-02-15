ee05bd68db1ad2cbb9600d4ecbd0a71cf7f73e74
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-02-15 19:54:20 +0100

    Ajout de la liste des catégories et création des filtres

--- FrontEnd/assets/travaux.js
+++ FrontEnd/assets/travaux.js
@@ -1,62 +1,110 @@
 // Paramètres d'appel à l'API
-let server_url="http://localhost:5678/api/";
-let state=0;
-let request_options={
-        method:"GET",
-        headers:{
-            'content-type':'application/json'
-        }
-    };
+let server_url = "http://localhost:5678/api/";
+let state = 0;
+let request_options = {
+    method: "GET",
+    headers: {
+        'content-type': 'application/json'
+    }
+};
 
-// Fonction qui permet de charger les travaux à partir du Backend
-function list_works(){
+main();
 
-    fetch(server_url+"works",request_options)
-    .then(function(response){
-        state=response.status;
-        return response.json();
-    })
-    .then(function(json){
-        console.log(json)
-        if(state==200){ // On vérifie que le serveur a bien répondu
-            let portfolio=document.getElementById('portfolio'); //Let portfolio = <section id="portfolio">
-            for(let i in json){ // On parcours le tableau de works renvoyé par l'API
-
-                // Création du titre de la catégorie si il n'a pas déjà été crée
-            let h2=document.getElementById('h2_'+json[i].category.id);
-            if(!h2){
-                 h2=document.createElement('h2');
-                 h2.setAttribute('id','h2_'+json[i].category.id);
-                 h2.textContent=json[i].category.name;
-                 portfolio.appendChild(h2);
-                }   
-
-                // Création du div gallery si il n'a pas déjà été crée
-            let div_gallery=document.getElementById('gallery_'+json[i].category.id);
-            if(!div_gallery){
-                div_gallery=document.createElement("div");
-                div_gallery.setAttribute('id','gallery_'+json[i].category.id);
-                div_gallery.setAttribute('class','gallery');             
-                portfolio.appendChild(div_gallery);
-            }
-
-            // On crée la figure pour le travail concerné
-            let figure=document.createElement('figure');
-            div_gallery.appendChild(figure);
-
-            // On crée l'image pour cette figure
-            let img=document.createElement('img');
-            img.setAttribute('src', json[i].imageUrl);
-            img.setAttribute('alt', json[i].title);
-            figure.appendChild(img);
-
-            // On crée le caption pour cette figure
-            let figcaption=document.createElement('figcaption');
-            figcaption.textContent=json[i].title;
-            figure.appendChild(figcaption);
-            }
-        }              
-    })
+async function main() {
+    await list_works();
+    await list_category();
 }
 
+// Fonction qui permet de charger les travaux à partir du Backend
+async function list_works(category_id) {
 
+    await fetch(server_url + "works", request_options)
+        .then(function (response) {
+            state = response.status;
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json)
+
+            // On vérifie que le serveur a bien répondu
+            if (state == 200) {
+
+                //Let portfolio = <section id="portfolio">
+                let portfolio = document.querySelector('.gallery');
+
+                // On efface tous les travaux pour avoir une page blanche
+                dropElement(portfolio);
+
+                // On parcours le tableau de works renvoyé par l'API
+                for (let i in json) {
+
+                    // On filtre sur la catégorie, si category_id est vide, on affiche tout
+                    if ((category_id == json[i].category.id) || (category_id == null)) {
+
+                        // On crée la figure 
+                        let figure = document.createElement('figure');
+
+                        // On crée l'image pour cette figure
+                        let img = document.createElement('img');
+                        img.setAttribute('src', json[i].imageUrl);
+                        img.setAttribute('alt', json[i].title);
+                        figure.appendChild(img);
+
+                        // On crée le caption pour cette figure
+                        let figcaption = document.createElement('figcaption');
+                        figcaption.textContent = json[i].title;
+                        figure.appendChild(figcaption);
+
+                        console.log(portfolio)
+                        portfolio.appendChild(figure)
+
+                    }
+                }
+            }
+        })
+}
+
+// Fonction qui permet de charger les catégories à partir du Backend
+async function list_category() {
+
+    await fetch(server_url + "categories", request_options)
+        .then(function (response) {
+            state = response.status;
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json)
+            if (state == 200) {
+                let filters = document.querySelector('.filters');
+                let filters_tous = filters.querySelector('li');
+                filters_tous.addEventListener('click', click_tous);
+                for (let i in json) {
+
+                    let elementFilters = document.createElement('li');
+                    elementFilters.textContent = json[i].name;
+                    elementFilters.setAttribute('data.id', json[i].id);
+                    elementFilters.addEventListener('click', click_filters);
+                    filters.appendChild(elementFilters);
+                }
+            }
+        })
+}
+async function click_filters(e) {
+    category_id = e.target.getAttribute('data.id');
+    await list_works(category_id);
+}
+
+async function click_tous(e) {
+    await list_works();
+}
+
+// Cette fonction permet d'effacer tous les éléments enfant d'un élément parent
+function dropElement(parent_element) {
+
+    // Tant qu'il y a au moins un enfant
+    while (parent_element.childNodes.length > 0) {
+
+        // On efface le dernier élément, le précédent devient le dernier, jusqu'à 0 enfants
+        parent_element.removeChild(parent_element.lastChild);
+    }
+}
