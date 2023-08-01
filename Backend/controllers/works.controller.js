c012a03541c70ee7c8972ab6be62b004a98ee943
Anylisse <121113301+Anylisse@users.noreply.github.com>
2023-08-01 17:21:11 +0200

    Mise à jour host backend render.com

--- Backend/controllers/works.controller.js
+++ Backend/controllers/works.controller.js
@@ -7,7 +7,8 @@ exports.findAll = async (req, res) =>  {
 }
 
 exports.create = async (req, res) => {
-	const host = req.get('host');
+	//const host = req.get('host');
+	const host = "https://portfolio-sophie-bluel-backend.onrender.com";
 	const title = req.body.title;
 	const categoryId = req.body.category;
 	const userId = req.auth.userId;
